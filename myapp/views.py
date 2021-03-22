7aa3632d05b75478e05967d593d1745e52c8b763
Misha <mishatambov@gmail.com>
2021-03-22 21:49:10 +0300

    создал bootstrap оболочку

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
+from django.views.generic import ListView
+
 from django.shortcuts import render
 from django.contrib.auth.models import User
+
+from myapp.models import Project, Data
+
+
+class TestView(ListView):
+    model = Project
+    template_name = 'index.html'
+    context_object_name = 'projects'
+    queryset = Project.objects.all()
 
