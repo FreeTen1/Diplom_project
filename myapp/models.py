9ef134c89631cf236cb9d3986c5bb20b5bf1a034
Misha <mishatambov@gmail.com>
2021-03-22 20:50:58 +0300

    создал модели
    User (автоматически созданная)
    Project (user, project_name, project_date_creation)
    Date (project, project_type, data, begin_heating, end_heating)

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=255, unique=True)
+    project_date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.project_name
+
+
+class Data(models.Model):
+    year = 'Y'
+    FIELD = [
+        (year, 'ежегодные данные'),
+    ]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project_type = models.CharField(max_length=1, choices=FIELD)
+    data = models.TextField()
+    begin_heating = models.DateField()
+    end_heating = models.DateField()
