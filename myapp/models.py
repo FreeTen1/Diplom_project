7f349286ac93075c7bf1b011dfe6f1ce42a9bf10
Misha <mishatambov@gmail.com>
2021-04-26 14:36:53 +0300

    локализовал allauth

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,18 @@
+# Выпускная работа бакалавра
+# студентки группы А-12-17
+# Торчкова Михаила Васильевича
+
+# Листинг модуля "models.py" приложения "myapp"
+
+# ==========
+
+# Импортируемые модули
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
 
+# класс, созданный по таблице Project
 class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     project_name = models.CharField(max_length=255, unique=True)
@@ -12,6 +22,7 @@ class Project(models.Model):
         return f"{self.id} {self.project_name} | {self.user}"
 
 
+# класс, созданный по таблице Data
 class Data(models.Model):
     month = 'M'
     FIELD = [
